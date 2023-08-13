ed43c328bdaa2923da251477804ff8ef255379c9
IgnacioBinaghi <binaghiignacio@gmail.com>
2023-08-13 18:07:31 -0400

    fixes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect, request
+from util.get_quote import get_quote
+import json
+import csv
+from util.get_new_workspaces import get_cafes
 
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask('__name__')
+
+
+@app.route("/home")
 def home():
-    return 'Hello, World!'
+    cafes = get_cafes()
+    return render_template("index.html", username = 'ignacio', cafes=cafes)
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    quote = get_quote()
+    if request.method == "POST":
+        name = request.form.get("name").lower()
+        email = request.form.get("email").lower()
+        password = request.form.get("password")
+
+    return render_template("login.html", quote=quote)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    quote = get_quote()
+    validity = ''
+    if request.method == "POST":
+        name = request.form.get("name").lower()
+        email = request.form.get("email").lower()
+        password = request.form.get("password")
+    return render_template("create_account.html", quote=quote)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ util/get_new_workspaces.py
@@ -0,0 +1,23 @@
+import requests
+import random
+
+API_KEY = 'ghsh_zLqfT86QRFc2mogqzoSZAZIJUPK8JEjhQ6LPXgGnnaN-2Y0-h1VzakNWk889cu1Kl3WXDgjhd6LPVjGaW9OwK3vuQ72DsnPiKaWuh2Ns-4iKa0tKRkEDL3TZHYx'
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': f'Bearer {API_KEY}'}
+PARAMETERS = {'term': 'cafe', 'location': 'New York City', 'limit': 50}
+
+response = requests.get(url=ENDPOINT, params=PARAMETERS, headers=HEADERS)
+businesses = response.json()['businesses']
+
+
+def get_cafes():
+    cafes = []
+    for business in businesses:
+        name = business['name']
+        address = ' '.join(business['location']['display_address'])
+        image_url = business['image_url']
+        url = business['url']
+        attending = random.randint(0, 10)
+        cafes.append([name, address, image_url, url, attending])
+    random.shuffle(cafes)
+    return cafes
