cb7a7dd764e6fedce9ecf534edf5072682e522fc
IgnacioBinaghi <binaghiignacio@gmail.com>
2023-08-14 19:57:13 -0400

    slay

---
+++ get_coord.py
@@ -0,0 +1,12 @@
+from geopy.geocoders import Photon
+geolocator = Photon(user_agent="measurements")
+
+def get_location(place):
+    location = geolocator.geocode(place)
+    if location:
+        return str(location)
+    else:
+        return None
+    
+
+print(get_location('brooklyn'))
